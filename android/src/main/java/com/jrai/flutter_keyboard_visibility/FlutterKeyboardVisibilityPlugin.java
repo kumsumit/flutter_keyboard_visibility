73aef62b37d80c3276bb5cc5695ae73e772e3295
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-09-14 22:32:57 +0530

    code updated

---
+++ android/src/main/java/com/jrai/flutter_keyboard_visibility/FlutterKeyboardVisibilityPlugin.java
@@ -0,0 +1,105 @@
+package com.jrai.flutter_keyboard_visibility;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.EventChannel;
+
+
+public class FlutterKeyboardVisibilityPlugin implements FlutterPlugin, ActivityAware, EventChannel.StreamHandler, ViewTreeObserver.OnGlobalLayoutListener {
+  private EventChannel.EventSink eventSink;
+  private View mainView;
+  private boolean isVisible;
+
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
+    init(flutterPluginBinding.getBinaryMessenger());
+  }
+
+  private void init(BinaryMessenger messenger) {
+    final EventChannel eventChannel = new EventChannel(messenger, "flutter_keyboard_visibility");
+    eventChannel.setStreamHandler(this);
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    unregisterListener();
+  }
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    listenForKeyboard(binding.getActivity());
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    unregisterListener();
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+    listenForKeyboard(binding.getActivity());
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    unregisterListener();
+  }
+
+  @Override
+  public void onListen(Object o, EventChannel.EventSink eventSink) {
+    this.eventSink = eventSink;
+  }
+
+  @Override
+  public void onCancel(Object o) {
+    this.eventSink = null;
+  }
+
+  @Override
+  public void onGlobalLayout() {
+    if (mainView != null) {
+      Rect r = new Rect();
+      mainView.getWindowVisibleDisplayFrame(r);
+
+      // check if the visible part of the screen is less than 85%
+      // if it is then the keyboard is showing
+      boolean newState = false;
+
+      if (android.os.Build.VERSION.SDK_INT >= 30){
+        newState = mainView.getRootWindowInsets().isVisible(WindowInsetsCompat.Type.ime());
+      } else{
+        // check if the visible part of the screen is less than 85%
+        // if it is then the keyboard is showing
+        newState = ((double)r.height() / (double)mainView.getRootView().getHeight()) < 0.85;
+      }
+      if (newState != isVisible) {
+        isVisible = newState;
+        if (eventSink != null) {
+          eventSink.success(isVisible ? 1 : 0);
+        }
+      }
+    }
+  }
+
+  private void listenForKeyboard(Activity activity) {
+    mainView = activity.<ViewGroup>findViewById(android.R.id.content);
+    mainView.getViewTreeObserver().addOnGlobalLayoutListener(this);
+  }
+
+  private void unregisterListener() {
+    if (mainView != null) {
+      mainView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+      mainView = null;
+    }
+  }
+}
